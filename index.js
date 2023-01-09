f82be0e96f5009fd05b62507112642f452a15b0e
rhythmicrhythm <Okubadejorh@gmail.com>
2023-01-09 20:50:49 +0100

    qr code added

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-const { Client } = require('whatsapp-web.js');
-const client = new Client();
+const fs = require('fs');
+const { Client, LegacySessionAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
 
-client.on('qr', (qr) => {
-    console.log('QR RECEIVED', qr);
+// Path where the session data will be stored
+const SESSION_FILE_PATH = './session.json';
+
+// Load the session data if it has been previously saved
+let sessionData;
+if(fs.existsSync(SESSION_FILE_PATH)) {
+    sessionData = require(SESSION_FILE_PATH);
+}
+
+// Use the saved values
+const client = new Client({
+    authStrategy: new LegacySessionAuth({
+        session: sessionData
+    })
 });
 
-client.on('ready', () => {
-    console.log('Client is ready!');
+// Save session values to the file upon successful auth
+client.on('authenticated', (session) => {
+    sessionData = session;
+    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (err) => {
+        if (err) {
+            console.error(err);
+        }
+    });
+});
+
+client.on('qr', qr =>{
+    qrcode.generate(qr, {small: true});
 });
 
 client.on('message', message => {
@@ -15,6 +38,9 @@ client.on('message', message => {
 	}
 });
 
-
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
 
 client.initialize();
+
