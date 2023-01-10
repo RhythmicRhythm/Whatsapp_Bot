3684926bd6fc441ae1e877b9bae37c36a1e6221c
rhythmicrhythm <Okubadejorh@gmail.com>
2023-01-10 21:47:40 +0100

    err server res

--- index.js
+++ index.js
@@ -1,32 +1,44 @@
 const fs = require('fs');
-const { Client, LegacySessionAuth } = require('whatsapp-web.js');
+const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
 // Path where the session data will be stored
 const SESSION_FILE_PATH = './session.json';
 
 // Load the session data if it has been previously saved
-let sessionData;
-if(fs.existsSync(SESSION_FILE_PATH)) {
-    sessionData = require(SESSION_FILE_PATH);
-}
+// let sessionData;
+// if(fs.existsSync(SESSION_FILE_PATH)) {
+//     sessionData = require(SESSION_FILE_PATH);
+// }
 
 // Use the saved values
 const client = new Client({
-    authStrategy: new LegacySessionAuth({
-        session: sessionData
-    })
-});
+    puppeteer: {
+      executablePath: '/usr/bin/brave-browser-stable',
+    },
+    authStrategy: new LocalAuth({
+      clientId: "client-one"
+    }),
+    puppeteer: {
+      headless: false,
+    }
+  });
 
 // Save session values to the file upon successful auth
+// client.on('authenticated', (session) => {
+//     sessionData = session;
+//     fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (err) => {
+//         if (err) {
+//             console.error(err);
+//         }
+//     });
+// });
+
 client.on('authenticated', (session) => {
-    sessionData = session;
-    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (err) => {
-        if (err) {
-            console.error(err);
-        }
-    });
-});
+    console.log('WHATSAPP WEB => Authenticated');
+  });
+
+
 
 client.on('qr', qr =>{
     qrcode.generate(qr, {small: true});
@@ -39,7 +51,7 @@ client.on('message', message => {
 });
 
 client.on('ready', () => {
-    console.log('Client is ready!');
+    console.log('Client is ready!');  
 });
 
 client.initialize();
